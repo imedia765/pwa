6e9ee0ec51687436b76fe2a8c48cc04f311d33ad
imedia765 <137381353+imedia765@users.noreply.github.com>
2024-12-18 01:12:40 +0000

    delete

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="text-center text-sm text-muted-foreground mt-12 pb-8">
-      © 2024 <span className="text-primary">SmartFIX Tech</span>, Burton Upon Trent. All rights reserved.
-    </footer>
-  );
-};
-
-export default Footer;
